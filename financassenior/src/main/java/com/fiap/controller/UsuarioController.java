5e6a7c7669f33690f889661d273563ff5f564a75
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2025-11-05 23:20:54 -0300

    Correção de erro 2

--- financassenior/src/main/java/com/fiap/controller/UsuarioController.java
+++ financassenior/src/main/java/com/fiap/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.net.URI;
 import java.util.List;
